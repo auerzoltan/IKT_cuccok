e7dff90ab45a7d4fc6e282f7c6fe35943c877ac5
auerzoltan <80397127+auerzoltan@users.noreply.github.com>
2022-04-04 13:36:21 +0200

    Add files via upload

---
+++ feladat1.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+foablak = Tk()
+foablak.title("Feladat")
+
+
+cimke1=Label(foablak, text="Első mező:")
+cimke1.grid(row=1, column=0)
+cimke2=Label(foablak, text="Második mező:")
+cimke2.grid(row=2, column=0)
+cimke3=Label(foablak, text="Harmadik:")
+cimke3.grid(row=3, column=0)
+
+mezo1=Entry(foablak)
+mezo1.grid(row=1, column=1, columnspan=1)
+mezo2=Entry(foablak)
+mezo2.grid(row=2, column=1, columnspan=1)
+mezo3=Entry(foablak)
+mezo3.grid(row=3, column=1, columnspan=1)
+
+can1= Canvas(foablak, width=160, height=160, bg="white")
+photo = PhotoImage(file ="villanykorte.png")
+item=can1.create_image(80, 80, image= photo)
+can1.grid(row=2, column=2, columnspan=3)
+
+foablak.mainloop()
